b9d20ab0e0414a57eb91710b891bba39b9e1d619
xain-1215 <74421245+xain-1215@users.noreply.github.com>
2022-05-13 17:16:03 +0800

    Base

--- app/src/main/java/com/example/firebaselogin/MainActivity.kt
+++ app/src/main/java/com/example/firebaselogin/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.firebaselogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var email: EditText
+    private lateinit var pwd: EditText
+    private lateinit var loginBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        email=findViewById(R.id.email)
+        pwd=findViewById(R.id.password)
+        loginBtn=findViewById(R.id.login)
+        loginBtn.setOnClickListener{
+            email.getText().toString()
+            pwd.getText().toString()
+        }
+        try{
+
+        }catch (e : Exception){
+            e.printStackTrace()
+        }
     }
 }
